6f2c9d9da4ae988784e536f50a71e6e4f9c12297
sharma830shruti <163344338+sharma830shruti@users.noreply.github.com>
2024-09-15 14:36:48 +0530

    Create leftRotateArrayByDplacesreversal.cpp

---
+++ leftRotateArrayByDplacesreversal.cpp
@@ -0,0 +1,20 @@
+
+void reverse(int arr[], int low, int high)
+{
+	while(low < high)
+	{
+		swap(arr[high], arr[low]);
+
+		low++;
+		high--;
+	}
+}   
+
+void leftRotate(int arr[], int d, int n)
+{
+	reverse(arr, 0, d - 1);
+
+	reverse(arr, d, n - 1);
+
+	reverse(arr, 0, n - 1);
+}
